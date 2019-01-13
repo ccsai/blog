8750bb3da5af5ddd09c871c03133dfb255d53b6a
chenchuan <1375738574@qq.com>
2019-01-13 21:58:59 +0800

    后垃圾数据清理加上事务

--- src/main/java/com/chenchuan/admin/resource/service/impl/JunkResouceServiceImpl.java
+++ src/main/java/com/chenchuan/admin/resource/service/impl/JunkResouceServiceImpl.java
@@ -4,6 +4,7 @@ import com.chenchuan.admin.resource.dao.JunkResouceDao;
 import com.chenchuan.admin.resource.service.JunkResouceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 垃圾数据清理service实现
@@ -16,12 +17,14 @@ public class JunkResouceServiceImpl implements JunkResouceService {
 
 
     @Override
+    @Transactional
     public int removeSecurityJunkData() {
         junkResouceDao.removeSecurityJunkData();
         return 1;
     }
 
     @Override
+    @Transactional
     public int removeBlogBusiJunkData() {
         junkResouceDao.removeBlogBusiJunkData();
         return 1;
